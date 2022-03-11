e4009743a5f2eae3fca21d5996696877177fee75
admin <fatimamahlaba@gmail.com>
2022-03-11 10:18:44 +0200

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://fatimamahlaba:UVVbmfcl6UrQCswW@cluster0.rwwo0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.log(error))
 db.once('open', () => console.log('Connected to Database'))
@@ -15,4 +15,4 @@ const subscribersRouter = require('./routes/subscribers')
 app.use('/subscribers', subscribersRouter)
 
 
-app.listen(3270, console.log('Server Started')) 
+app.listen(3271, console.log('Server Started')) 
