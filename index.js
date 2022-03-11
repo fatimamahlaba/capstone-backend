a9afc8d52f59e805a3d08bc84aec6cbc942dd474
admin <fatimamahlaba@gmail.com>
2022-03-11 13:02:45 +0200

    changes

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const mongoose = require('mongoose')
+
+app.use(express.json())
+app.use(cors())
 
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.log(error))
 db.once('open', () => console.log('Connected to Database'))
- 
-app.use(express.json())
 
 app.get('/', (req, res) => {
+    
     res.send('Fatima Api')
 })
 
 const subscribersRouter = require('./routes/subscribers')
 app.use('/subscribers', subscribersRouter)
 
-const port = app.set(process.env.PORT || 3271)
+const port = app.set('port', process.env.PORT || 3270)
 
-app.listen(port, console.log(`Server Started on 3271`)) 
+app.listen(port, console.log(`Server Started on ${app.get('port')}`)) 
